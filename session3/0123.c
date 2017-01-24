ac3fbabd0f13002f0d3e1304c12a61f8cda1b3a2
Saeko Fukui <saeko0032@gmail.com>
2017-01-23 18:18:37 -0800

    class note for anuary 3rd

--- session3/0123.c
+++ session3/0123.c
@@ -1,22 +1,48 @@
-//
-//  main.c
-//  assignments2-6
-//
-//  Created by saekof on 2017-01-19.
-//  Copyright © 2017 CICCC. All rights reserved.
-//
+// January 3rd
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
-
-void swapNumbers(int number1, int number2)
+// for-loop
+for ( initialization; condition;update)
 {
-    int tempNumber = number1;
-    number1 = number2;
-    number2 = tempNumber;
+   for-body;
 }
+// while-loop
+while(condition)
+{
+    while-body;
+}
+
+///////when we use for-loop
+// -> we need to understand how many iterate we will do
+//////when we use while-loop
+// -> we don't know how many times loop is going to happen
+
+// Arrays
+//   |_ Data structure
+//   |_store a collection of 
+//   |
+//   |_variavle, with the same type
+//   |_size in fined.
+       
+// if you want to use 5integer,
+// it's better to declare arrays
+// because array allocate memory continuously
+ ______________________
+[ 5 | 11 | 7 | 2 | 20 ]
+ [0] [1]  [2] [3]  [4]
+ 
+int rowH = number[1]*number[4];
+//  220  +    11    *    20;
+
+int result = number[5] + number[5];
+             // random number
+// if you didn't allocate and initialize the array index,
+// we don't know the number of this index.
+// and c didn't throw any errors.
+
+// we must declare how many memory we want use in C.
+int number[10]; //ID array
+int number[] = {5,6,10,20,-1,6};
+
+int char[4] = {'a','b','c','d','e','f'}
+//             [0] [1] [2] [3] [4] [5] -< we will over write [5] address
+
