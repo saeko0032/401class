82e18cd8df55724068f4cdb566990ca40fedca92
Saeko Fukui <saeko0032@gmail.com>
2017-01-24 14:38:23 -0800

    Create assignments3-5.c

---
+++ session3/assignments3-5.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  problem3-5
+//
+//  Created by saeko fukui on 2017-01-24.
+//  Copyright © 2017 Saeko Fukui. All rights reserved.
+//
+
+#include <stdio.h>
+
+void countOddAndEven(int originArrays[5]);
+
+int main(int argc, const char * argv[]) {
+    int orgArrays[5] = {32, 58, 3, 7, 4};
+    
+    countOddAndEven(orgArrays);
+    return 0;
+}
+void countOddAndEven(int originArrays[5])
+{
+    int oddCounter = 0;
+    for(int i = 0; i < 5; i++)
+    {
+        if (originArrays[i] % 2 == 1) oddCounter++;
+    }
+    printf("you have %d odd numbers and %d even numbers\n",oddCounter, 5 - oddCounter);
+
+}
