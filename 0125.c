387a1bae0c6b5906ef7f9b3f4c89e718954ed74b
Saeko Fukui <saeko0032@gmail.com>
2017-01-25 19:57:54 -0800

    Create 0125.c

---
+++ 0125.c
@@ -0,0 +1,22 @@
+// pointer
+int number = 5;
+printf("The address is %P", &number);
+int* address = &number; // address も同じ場所を指し示せる
+
+int arrys[4] = 4 * 4 = 16 bytes;
+char word[4] = 1 * = 4 bytes
+
+int number = 5;
+&number = 2; // address
+*(&number) = 5; // value
+int* pinter = 6;
+&pointer = 10; // address
+*(&pointer) = 6;
+________________
+[0______|1______]
+[2//////|3//////]
+[4//////|5//////]
+[6______|7______]
+[8______|9______]
+[10\\\\\|11\\\\\]
+[12\\\\\|13\\\\\]
