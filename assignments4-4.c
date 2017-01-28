13d20fe8d4dc4584ffe4f0c1205bde209b3de74a
Saeko Fukui <saeko0032@gmail.com>
2017-01-27 17:34:53 -0800

    Update assignments4-4.c

--- assignments4-4.c
+++ assignments4-4.c
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[]) {
     
     for(int i = 0; i < 5; i++)
     {
-        printf("%d\n",*pointer);
+        printf("%d\n", *pointer);
         pointer ++;
     }
     return 0;
