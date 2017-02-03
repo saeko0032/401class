3dc5332aebfc10656055901bf3374bcbfa184223
Saeko Fukui <saeko0032@gmail.com>
2017-02-02 19:01:20 -0800

    Update assignments6-1.c

--- session6/assignments6-1.c
+++ session6/assignments6-1.c
@@ -1,25 +1,48 @@
 //
 //  main.c
-//  assignments5-1
+//  assignments6-1
 //
-//  Created by saekof on 2017-01-30.
+//  Created by saekof on 2017-01-31.
 //  Copyright © 2017 CICCC. All rights reserved.
 //
 
 #include <stdio.h>
 #include <stdlib.h>
-
-struct date{
-    int day;
-    int month;
-    int year;
-};
+void increase(int data[], int counter);
 
 int main(int argc, const char * argv[]) {
-    struct date *dates;
-    int num;
-    dates = (struct date*) malloc(num * sizeof(struct date));
-    dates->day = 10;
-    printf("date : %d\n",dates->day);
-    return 0;
+    int flg = 1;
+    int counter = 0;
+    char arr[10];
+    int *point = malloc(1 * sizeof(int));
+    while (flg) {
+        printf("enter the number you want(except 0)\n");
+        
+        fgets(arr, 4, stdin);
+        point[counter] = atoi(arr);
+
+        if (0 >= point[counter]) {
+            flg = 0;
+            int MaxNumber = point[0];
+            for(int j = 0; j <counter; j++)
+            {
+                if(point[j] < point[j+1])
+                {
+                    MaxNumber = point[j+1];
+                }
+            }
+            printf("maxNumber is %d\n",MaxNumber);
+        }
+        else
+        {
+            counter++;
+            increase(point, counter);
+        }
+    }
+
+}
+
+void increase(int data[], int counter)
+{
+    data = realloc(data, (counter + 1) * sizeof *data);
 }
